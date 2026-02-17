efacbcef985094406357d8e71c451610bac06519
Vitalii <v.hulaievych@gmail.com>
2026-02-17 19:13:54 +0000

    solution

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,18 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it('should be case-insensitive', () => {
+    expect(isIsogram('Adam')).toBe(false);
+  });
+
+  it('should be right result for empty string', () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it('should be right result for different situation', () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+    expect(isIsogram('look')).toBe(false);
+    expect(isIsogram('Oops')).toBe(false);
+  });
 });
